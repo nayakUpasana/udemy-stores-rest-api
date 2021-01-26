c263fe591ae4622cd3f266644c87f73f69249a0a
Upasana Nayak <nayak_upasana@Upasanas-MacBook-Pro.local>
2021-01-26 15:10:03 -0600

    Fixed not importing db when running the app.

--- app.py
+++ app.py
@@ -17,12 +17,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'moon'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)      # /auth
 
 api.add_resource(Item, '/item/<string:name>')     # http://127.0.0.1:5000/item/piano
